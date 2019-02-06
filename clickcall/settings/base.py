510cf750fc0253980c21720c464ec7cf23528f9b
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2019-02-06 17:36:59 -0300

    Configuração do django_heroku

--- clickcall/settings/base.py
+++ clickcall/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -124,3 +125,6 @@ STATICFILES_DIRS = [
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# Django-heroku configuration
+django_heroku.settings(locals())
