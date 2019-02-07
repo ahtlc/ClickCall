80649cb325e7855f5521ca4d06fe59428d731731
EulaliaAires <memgra@cin.ufpe.br>
2019-02-07 00:22:04 -0300

    feature(login-frontend) add login template and add static root in base settings

---
+++ static/js/show-password.js
@@ -0,0 +1,18 @@
+
+function showPassword() {
+  var element = document.getElementById("pwd");
+  var element2 = document.getElementById("eye");
+
+  if(pwd.value != ""){
+
+    if (pwd.type === "password") {
+      pwd.type = "text";
+      element2.className = "show2";
+      document.getElementById("eye").src= "../img/eye-open.png";
+    } else {
+      pwd.type = "password";
+      element2.className = "show";
+      document.getElementById("eye").src= "../img/eyes-closed.png";
+    }
+  }
+}
