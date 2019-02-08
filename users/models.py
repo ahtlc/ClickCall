883c7e1a3eee8c4e9474651afc19f8932da65a7f
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2019-02-08 09:38:52 -0300

    feature(auth-system-backend): created custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .manager import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom model to users of clickcall
+    """
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(unique=True, max_length=50,
+                                default='Undefined')
+    avatar = models.ImageField(blank=True, default='default.avatar.png')
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
