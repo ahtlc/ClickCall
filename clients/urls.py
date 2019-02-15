f92fe082ea13e7ab3dcf41e73dd6282724521c05
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2019-02-15 10:21:20 -0300

    feature(new-contact-front): moved template to activities app

--- clients/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import NewContactView
-
-app_name='clients'
-
-urlpatterns = [
-    path('new', NewContactView.as_view(), name='new_contact'),
-]
--- clients/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-
-
-class NewContactView(generic.TemplateView):
-    template_name='tests.html'
