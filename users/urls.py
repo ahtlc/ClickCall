948c57b17a1b1dfbd462beb4a0073f6286e3b43b
eulaliaaires <eulaliaaires@gmail.com>
2019-02-15 12:02:03 -0300

    feature(login-frontend) include url

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
-
 from django.urls import path
+from .views import TestView
+
 app_name = 'users'
 
 urlpatterns = [
     # path('test/', )
-    path('test', TestView.as_view(), name = "test"),
+    path('test/', TestView.as_view(), name = "test"),
 ]
