422c4f7ab9d30255f9490a3379bf7ab3c0c66efb
eulaliaaires <eulaliaaires@gmail.com>
2019-02-15 13:42:51 -0300

    feature(login-frontend) change url and replace images path

--- clickcall/urls.py
+++ clickcall/urls.py
@@ -23,9 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('activities/', include('activities.urls')),
     path('users/', include('users.urls')),
-    # path('test/', )
 ]
-
 
 
 if settings.DEBUG:
