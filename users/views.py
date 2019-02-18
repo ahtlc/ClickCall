a4d69a0fe1a38ffe2317e62fe94ffe4cb4037ade
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2019-02-18 11:11:21 -0300

    feature(auth-system-backend): create users' app views

--- users/views.py
+++ users/views.py
@@ -1,15 +1,32 @@
 from django.views import generic
+from django.contrib import auth
+from django.contrib.auth import get_user_model, authenticate, views
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+
+from .models import User
 
 
-class PasswordTestView (generic.TemplateView):
-    template_name = "password-reset.html"
+class PasswordResetView (generic.TemplateView):
+    template_name = 'password-reset.html'
 
 
-class TestView (generic.TemplateView):
-    template_name = "login.html"
+class ConfirmPasswordResetView (generic.TemplateView):
+    template_name = 'confirm-password-reset.html'
 
 
-class ConfirmPasswordView (generic.TemplateView):
-    template_name = "confirm-password-reset.html"
+class LogoutView(views.LogoutView):
+    next_page = 'login'
 
-# Create your views here.
+
+class LoginView(views.LoginView):
+    success_url = 'common:test'
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('common:home'))
+        return super(LoginView, self).get(request)
+
