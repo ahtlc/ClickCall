eb8d8f25724c4c9dccae172f541ef8ff7c406c43
eulaliaaires <memgra@cin.ufpe.br>
2019-02-19 17:23:59 -0300

    feature(contact-model) add contact class in models.py

--- calls/models.py
+++ calls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import os
+from __future__ import unicode_literals
 
+
+class Contact(models.Model):
+    name = models.CharField(max_length = 80, blank=False)
+    company_name = models.CharField(max_length = 40, blank=False)
+    email = models.EmailField(max_length = 256, blank=False)
+    cellphone = models.CharField(max_length = 11)
+    phone = models.CharField(max_length = 10)
+    adress = models.CharField(max_length = 256)
+    tag = models.ForeignKey('Tag', on_delete = models.CASCADE)
 # Create your models here.
+
+class Tag(models.Model):
+
