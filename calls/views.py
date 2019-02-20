2fc7c866900ecfdd31566015c1f2ede5379f62ab
EulaliaAires <eulaliaaires@gmail.com>
2019-02-20 16:34:59 -0300

    feature(contact-backend) add contact form and register contact in admin.py

--- calls/views.py
+++ calls/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from .models import Contact
 from .forms import contactForm
 
-def contact_new(request):
+  def contact_new(request):
     if request.method == "POST":
-         form = contactForm(request.POST)
-         if form.is_valid():
-             contact = form.save(commit=False)
-             contact.save()
-    else:
-        form = contactForm()
-    return render(request, '.html', {'form': form})
+      form = contactForm(request.POST)
+      if form.is_valid():
+        contact = form.save(commit=False)
+        contact.save()
+    else: 
+      form = contactForm()
+    return render (request, '.html',{'form': form})
+
