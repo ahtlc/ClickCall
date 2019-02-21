95bda42b6d11ab8f8c2f93728cef3038c964c5e4
EulaliaAires <eulaliaaires@gmail.com>
2019-02-21 13:37:17 -0300

    feature(contact-model) register admin

--- calls/admin.py
+++ calls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Contact, Call, Tag, Subject
 # Register your models here.
+admin.site.register(Contact)
+admin.site.register(Call)
+admin.site.register(Tag)
+admin.site.register(Subject)
--- calls/models.py
+++ calls/models.py
@@ -17,17 +17,51 @@ class Contact(models.Model):
   phone_regex = RegexValidator(regex=r'^\(\d{2}\)\d{4}-\d{4}$', message="O número deve ser cadastrado da seguinte forma: (DD) 9999-9999")
   email_regex = RegexValidator(regex=r'^([\w\-]+\.)*[\w\- ]+@([\w\- ]+\.)+([\w\-]{2,3})$', message="O email deve ser cadastrado da seguinte forma: email@email.com")
 
-  name = models.CharField(max_length = 80, blank=False, verbose_name = "Nome")
+  name = models.CharField(max_length = 256, blank=False, verbose_name = "Nome")
   company_name = models.CharField(max_length = 40, blank=False, verbose_name = "Empresa")
   email = models.EmailField(max_length = 256, blank=False, validators = [email_regex], verbose_name = "Email do contato")
   cellphone = models.CharField(max_length = 11, validators = [cellphone_regex], verbose_name = "Celular")
   phone = models.CharField(max_length = 10, validators = [phone_regex], verbose_name = "Telefone")
   adress = models.CharField(max_length = 256, verbose_name = "Endereço")
   last_update = models.DateTimeField(auto_now = False, verbose_name = "Última atualização")
-  status = models.CharField(max_length = 8 ,choices = STATUS_CHOICES, default = ACTIVE, verbose_name = "Status")
+  status = models.CharField(max_length = 8, choices = STATUS_CHOICES, default = ACTIVE, verbose_name = "Status")
   tag = models.ForeignKey('Tag', on_delete = models.CASCADE, verbose_name = "Tag")
+
+  class Meta:
+        verbose_name_plural = "Contatos"
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
-  name = models.Charfield(max_length = 50)
+  name = models.Charfield(max_length = 50, primary_key = True, verbose_name = "Nome")
+
+  class Meta:
+        verbose_name_plural = "Tags"
+    def __str__(self):
+        return self.name
+
+class Call(models.Model):
+
+  call_id = models.AutoField(primary_key = True, verbose_name = "Número da Ligação")
+  duration = models.TimeField(auto_now = False , verbose_name = "Duração")
+  call_24_hours = models.BooleanField(default = False)
+  email_sended = models.BooleanField(default = False)
+  paid = models.BooleanField(default = False)
+  notes = models.TextField(max_length = 256)
+  subject = models.ForeignKey('Subject', on_delete = models.CASCADE, verbose_name = "Assunto")
+
+  class Meta:
+        verbose_name_plural = "Ligações"
+    def __str__(self):
+        return self.call_id
+
+class Subject(models.Model):
+  name = models.CharField(max_length = 256, primary_key = True, verbose_name = "Nome")
+
+  class Meta:
+        verbose_name_plural = "Assuntos"
+    def __str__(self):
+        return self.name
+
 
