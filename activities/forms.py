e2e24ebd77d792f8168e8d0128fd2f3e67351696
Júnior Mendes <jrmmendes@outlook.com>
2019-03-13 15:36:49 -0300

    feature(contact-backend): add custom save() method

--- activities/forms.py
+++ activities/forms.py
@@ -1,10 +1,25 @@
 from django import forms
-from calls.models import Contact
+from calls.models import Contact, Tag
 
-class contactForm(forms.ModelForm):
 
-  class Meta:
-    model = Contact
-    fields = '__all__'
+class ContactForm(forms.ModelForm):
+    """
+    Form that handles the contact creation
+    """
+    raw_tags = forms.CharField(widget=forms.HiddenInput())
 
-  
+    def save(self, commit=True):
+        tags_name = self.cleaned_data.get('raw_tags').split(',')
+ 
+        # TODO: create the m2m relationship
+        for tag_name in tags_name:
+            if Tag.objects.filter(name=tag_name).exists():
+                Tag.objects.create(
+                    name=tag_name)
+
+        return super().save()
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
+        exclude = ['tag', 'last_update', 'status']
