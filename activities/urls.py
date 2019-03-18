395d21c796671552880b6bf9729d49f6979b9977
Marco Santana <m.aurelio_santana@hotmail.com>
2019-03-18 11:06:42 -0300

    feature(collab-history) rebuild branch and finished front+back collaborator history

--- activities/urls.py
+++ activities/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import TestView
+from .views import (TestView,
+                    HistoryCollaboratorView
+                    )
 
 app_name='activities'
 
 urlpatterns = [
     path('test/', TestView.as_view(), name="test"),
+    path('test-collaborator/', HistoryCollaboratorView.as_view(),name="test-history")
 ]
