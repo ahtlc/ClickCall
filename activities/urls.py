a8f535fd96b43e7d6c2396812cb6027f1ddf0336
unknown <ahtlc@cin.ufpe.br>
2019-03-21 23:58:02 -0300

    erro view to fix

--- activities/urls.py
+++ activities/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TestView
+from .views import TestView, SystemMetrics
 
 app_name='activities'
 
 urlpatterns = [
     path('test/', TestView.as_view(), name="test"),
+    path('metricas/', SystemMetrics.as_view(), name="metricas"),
 ]
--- activities/views.py
+++ activities/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import generic
+from django.views.generic import TemplateView
 
 
 class TestView(generic.TemplateView):
     template_name = "call-history.html"
+
+class SystemMetrics(TemplateView):
+    template_name = "system-metrics.html"
