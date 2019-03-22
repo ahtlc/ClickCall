899efa63aa212706459d351a3135990ab88b9d8d
lucasmorais6 <lma6@cin.ufpe.br>
2019-03-22 16:42:19 -0300

    divs almost done

--- activities/urls.py
+++ activities/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import TestView, SystemMetrics
+from .views import SystemMetrics
 
 app_name='activities'
 
 urlpatterns = [
-    path('test/', TestView.as_view(), name="test"),
-    path('metricas/', SystemMetrics.as_view(), name="metricas"),
+    path('metrics/', SystemMetrics.as_view(), name="metrics"),
 ]
