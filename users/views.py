4eb3ad86e788867efdbbd56c743a2ae5bb3d8e2b
Júnior Mendes <jrmmendes@outlook.com>
2019-03-24 17:24:44 -0300

    feature(auth-system-backend): Refactor views

--- users/views.py
+++ users/views.py
@@ -1,18 +1,18 @@
 from django.views import generic
 from django.contrib.auth import views as auth_views
-from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth import get_user_model
 from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
+
 from .models import User
 from .forms import SignupForm
-from django import forms
 
 
 class SignupView(generic.FormView):
     form_class = SignupForm
     success_url = 'users:login'
-    template_name = 'signup.html'
+    template_name = 'users/signup.html'
     redirect_authenticated_user = True
 
     model = get_user_model()
@@ -27,15 +27,6 @@ class SignupView(generic.FormView):
         return redirect('users:login')
 
 
-class PasswordResetView(auth_views.PasswordResetView):
-    template_name = 'password-reset.html'
-    form_class = forms.PasswordChangeForm
-
-
-class ConfirmPasswordResetView (generic.TemplateView):
-    template_name = 'confirm-password-reset.html'
-
-
 class LogoutView(auth_views.LogoutView):
     next_page = 'users:login'
 
@@ -44,16 +35,12 @@ class LoginView(auth_views.LoginView):
     """
     View that handles the login process
     """
-    success_url = 'users:profile'
-    template_name = 'login.html'
-    redirect_authenticated_user = False
-
-    def get(self, request, *arg, **kwargs):
-        if(request.user.is_authenticated):
-            return HttpResponseRedirect(reverse_lazy('users:profile'))
-        return super(LoginView, self).get(request)
+    success_url = reverse_lazy('users:profile')
+    template_name = 'users/login.html'
+    redirect_authenticated_user = True
+    redirect_field_name = success_url
 
 
 class ProfileView(generic.TemplateView):
-    template_name = 'profile.html'
+    template_name = 'users/profile.html'
     model = User
