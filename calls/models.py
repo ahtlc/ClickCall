5931baaca4aaa4942662c1d202854286d745a864
Júnior Mendes <jrmmendes@outlook.com>
2019-04-03 15:03:43 -0300

    Rebase from develop

--- calls/models.py
+++ calls/models.py
@@ -8,72 +8,84 @@ class Contact(models.Model):
     ACTIVE = 'AC'
     INACTIVE = 'IN'
     STATUS_CHOICES = (
-          ('ACTIVE', 'Ativo'),
-          ('INACTIVE', 'Inativo'),
+        ('ACTIVE', 'Ativo'),
+        ('INACTIVE', 'Inativo'),
     )
 
     cellphone_regex = RegexValidator(
-            regex=r'^\(\d{2}\)\d{1}-\d{4}-\d{4}$',
-            message="O número deve ser cadastrado da seguinte forma:\
-                    (DD)9-9999-9999")
+        regex=r'^\(\d{2}\)\d{1}-\d{4}-\d{4}$',
+        message="O número deve ser cadastrado da seguinte forma:\
+                (DD)9-9999-9999"
+    )
 
     phone_regex = RegexValidator(
-            regex=r'^\(\d{2}\)\d{4}-\d{4}$',
-            message="O número deve ser cadastrado da seguinte forma:\
-                    (DD)9999-9999")
+        regex=r'^\(\d{2}\)\d{4}-\d{4}$',
+        message="O número deve ser cadastrado da seguinte forma:\
+                (DD)9999-9999"
+    )
 
     email_regex = RegexValidator(
-            regex=r'^([\w\-]+\.)*[\w\- ]+@([\w\- ]+\.)+([\w\-]{2,3})$',
-            message="O email deve ser cadastrado da seguinte forma:\
-                     email@email.com")
+        regex=r'^([\w\-]+\.)*[\w\- ]+@([\w\- ]+\.)+([\w\-]{2,3})$',
+        message="O email deve ser cadastrado da seguinte forma:\
+                 email@email.com"
+    )
 
     name = models.CharField(
-            max_length=256,
-            blank=False,
-            verbose_name="Nome")
+        max_length=256,
+        blank=False,
+        verbose_name="Nome"
+    )
 
     company_name = models.CharField(
-            max_length=40,
-            blank=False,
-            verbose_name="Empresa")
+        max_length=40,
+        blank=False,
+        verbose_name="Empresa"
+    )
 
     email = models.EmailField(
-            max_length=256,
-            blank=False,
-            validators=[email_regex],
-            verbose_name="Email")
+        max_length=256,
+        blank=False,
+        validators=[email_regex],
+        verbose_name="Email"
+    )
 
     cellphone = models.CharField(
-            max_length=20,
-            validators=[cellphone_regex],
-            verbose_name="Celular",
-            help_text="O número deve ser cadastrado da seguinte forma:\
-                      (DD)9-9999-9999")
+        max_length=20,
+        validators=[cellphone_regex],
+        verbose_name="Celular",
+        help_text="O número deve ser cadastrado da seguinte forma:\
+                  (DD)9-9999-9999"
+    )
 
     phone = models.CharField(
-            max_length=20,
-            validators=[phone_regex],
-            verbose_name="Telefone",
-            help_text="O número deve ser cadastrado da seguinte forma:\
-                    (DD)9999-9999")
+        max_length=20,
+        validators=[phone_regex],
+        verbose_name="Telefone",
+        help_text="O número deve ser cadastrado da seguinte forma:\
+                (DD)9999-9999"
+    )
 
     adress = models.CharField(
-            max_length=256,
-            verbose_name="Endereço")
+        max_length=256,
+        verbose_name="Endereço"
+    )
 
     last_update = models.DateTimeField(
-            auto_now=False,
-            verbose_name="Última atualização")
+        auto_now=False,
+        verbose_name="Última atualização"
+    )
 
     status = models.CharField(
-            max_length=8,
-            choices=STATUS_CHOICES,
-            default=ACTIVE,
-            verbose_name="Status")
+        max_length=8,
+        choices=STATUS_CHOICES,
+        default=ACTIVE,
+        verbose_name="Status"
+    )
 
     tag = models.ManyToManyField(
-            'Tag',
-            verbose_name="Tag")
+        'Tag',
+        verbose_name="Tag"
+    )
 
     class Meta:
         verbose_name = "Contato"
@@ -85,9 +97,10 @@ class Contact(models.Model):
 
 class Tag(models.Model):
     name = models.CharField(
-            max_length=50,
-            primary_key=True,
-            verbose_name="Nome")
+        max_length=50,
+        primary_key=True,
+        verbose_name="Nome"
+    )
 
     class Meta:
         verbose_name = "Tag"
@@ -100,50 +113,50 @@ class Tag(models.Model):
 class Call (models.Model):
 
     call_id = models.AutoField(
-        primary_key = True,
-        verbose_name = "Número da Ligação
+        primary_key=True,
+        verbose_name="Número da Ligação"
     )
-    
+
     duration = models.TimeField(
-        auto_now = False,
-        verbose_name = "Duração"
+        auto_now=False,
+        verbose_name="Duração"
     )
-    
+
     call_24_hours = models.BooleanField(
-        default = False,
-        verbose_name = "Chamada adiada"
+        default=False,
+        verbose_name="Chamada adiada"
     )
-    
+
     email_sended = models.BooleanField(
-        default = False,
-        verbose_name = "Email enviado"
+        default=False,
+        verbose_name="Email enviado"
     )
-    
+
     paid = models.BooleanField(
-        default = False,
-        verbose_name = "Pagamento efetuado"
+        default=False,
+        verbose_name="Pagamento efetuado"
     )
-    
-    contact =  models.ForeignKey(
+
+    contact = models.ForeignKey(
         'Contact',
-        on_delete = models.CASCADE,
-        verbose_name = "Contato"
+        on_delete=models.CASCADE,
+        verbose_name="Contato"
     )
-    
+
     notes = models.TextField(
-        max_length = 256,
-        verbose_name = "Anotações"
+        max_length=256,
+        verbose_name="Anotações"
     )
-    
+
     subject = models.ManyToManyField(
         'Subject',
-        verbose_name = "Assunto",
-        max_length = 3
+        verbose_name="Assunto",
+        max_length=3
     )
-    
+
     date = models.DateField(
         default=datetime.date.today,
-        verbose_name = "Data da Ligação"
+        verbose_name="Data da Ligação"
     )
 
     class Meta:
@@ -152,14 +165,16 @@ class Call (models.Model):
 
     def __str__(self):
         return self.contact.name + " " + datetime.datetime.strftime(
-                self.contact.last_update, "%d/%m/%Y")
+            self.contact.last_update, "%d/%m/%Y"
+        )
 
 
 class Subject(models.Model):
     name = models.CharField(
-            max_length=256,
-            primary_key=True,
-            verbose_name="Nome")
+        max_length=256,
+        primary_key=True,
+        verbose_name="Nome"
+    )
 
     class Meta:
         verbose_name = "Assunto"
--- common/views.py
+++ common/views.py
@@ -2,9 +2,5 @@ from django.views import generic
 from django.urls import reverse_lazy
 
 
-class TestView(generic.TemplateView):
-    template_name = 'login.html'
-
-
 class IndexView(generic.RedirectView):
     url = reverse_lazy('users:login')
