7002f57a004bc1b791967e5aa4f3510b0364081d
Romildo Júnior <jrmmendes@outlook.com>
2019-06-01 15:48:37 -0300

    Add tags backend

--- activities/views.py
+++ activities/views.py
@@ -29,7 +29,11 @@ class PopulateView(generic.View):
     def get(self, request, *args, **kwargs):
         return create()
 
+
 class SystemMetricsView(generic.TemplateView):
+    """
+    View to render the system metrics
+    """
     template_name = "system-metrics.html"
 
 
@@ -188,6 +192,13 @@ class ClientsView(CreateView):
         return context
 
     def form_valid(self, form):
+        form.save()
+        raw_tags = form.cleaned_data['raw_tags'].split(',')
+
+        for raw_tag in raw_tags:
+            tag = Tag.objects.filter(name=raw_tag)
+            form.instance.tag.add(tag.get(name=raw_tag))
+
         return super(ClientsView, self).form_valid(form)
 
     def form_invalid(self, form):
