d0fe01cee669eef4e26ea11d8bdd9bc06f25bf75
Romildo Júnior <jrmmendes@outlook.com>
2019-06-01 14:21:34 -0300

    Fix app viewport display property

--- activities/urls.py
+++ activities/urls.py
@@ -15,13 +15,12 @@ urlpatterns = [
     path('test-history/', HistoryActivitiesView.as_view(), name="test-history"),
     path('getdata/', GetTotalCallsView.as_view(), name="getdata"),
     path('schedule/', ScheduleView.as_view(), name="schedule"),
-    path('populate_db/', PopulateView.as_view(), name="populate_db"),
-    path('system_metrics', SystemMetricsView.as_view(), name="system_metrics")
+    path('system_metrics/', SystemMetricsView.as_view(), name="system_metrics")
 ]
 
 from django.conf import settings
 
 if(settings.DEBUG):
-  urlpatterns+=[
-    path('populate_db/', PopulateView.as_view(), name="populate_db")
+    urlpatterns += [
+        path('populate_db/', PopulateView.as_view(), name="populate_db")
     ]
