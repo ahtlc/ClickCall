418311686bda935092f84151371e269480c22f61
Romildo Júnior <jrmmendes@outlook.com>
2019-06-28 21:52:49 -0300

    Move django-heroku to production settings

--- clickcall/settings/production.py
+++ clickcall/settings/production.py
@@ -1,6 +1,10 @@
 from .base import *
+import django_heroku
 
 DEBUG = False
 SECURE_SSL_REDIRECT = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Django-heroku configuration
+django_heroku.settings(locals())
