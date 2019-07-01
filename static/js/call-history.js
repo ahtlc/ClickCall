d57e1641b08f775e73e37cf7840e69ac1cbe636c
eulaliaaires <eulaliaaires@gmail.com>
2019-07-01 16:15:39 -0300

    hotfix(call-detail) show tags in detail

--- static/js/call-history.js
+++ static/js/call-history.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     let pk;
     let flag = -1;
     let currentPk;
+    let clickActive = 0;
     $(".table-row").click(function (e) {
       if (pk === e.currentTarget.getAttribute(`data-pk`) || !pk || flag === -1) {
         if (!$(e.target).hasClass('icon-call-2') && !$(e.target).hasClass('icon-pay-2') && !$(e.target).hasClass('icon-email-2')) {
@@ -10,7 +11,28 @@ $(document).ready(function () {
           flag *= -1;
         }
       }
-      pk = e.currentTarget.getAttribute(`data-pk`);
+        pk = e.currentTarget.getAttribute(`data-pk`);
+        $.ajax({
+          url: "/activities/schedule/tag",
+          type: "get", // or "get"
+          data: {
+              pk:pk,
+          },
+          success: function(context){
+            tag = context.tags
+            console.log(tag);
+              $('.tag-row').empty()
+              for(var i = 0; i < tag.length; i++){
+                  document.querySelector('.tag-row').innerHTML += ` <div get-data="data-tag"class="tag-box">${tag[i].name}</div>`
+              }
+            
+              // var elem = document.getElementById('tag-row');
+              // elem.parentNode.removeChild(elem);
+          
+          console.log(clickActive);
+        },
+          
+      });
       const inputs = ["company", "name", "cellphone", "email", "value", "pk", "date", "tag", "notes"];
       inputs.forEach((input) => {
         $(`span[get-data=data-${input}]`).html(e.currentTarget.getAttribute(`data-${input}`));
